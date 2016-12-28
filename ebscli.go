89e374c9038fd7c27a7ba555608972b64109c91c
dnABic (Andreja Babic) <ababic@gmail.com>
2016-12-28 01:10:42 +0100

    Test example

---
+++ ebscli.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func main() {
+	sess, err := session.NewSession()
+	if err != nil {
+		panic(err)
+	}
+
+	svc := ec2.New(sess, &aws.Config{Region: aws.String("us-east-1")})
+
+	resp, err := svc.DescribeInstances(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("> Number of reservation sets: ", len(resp.Reservations))
+	for idx, res := range resp.Reservations {
+		fmt.Println("  > Number of instances: ", len(res.Instances))
+		for _, inst := range resp.Reservations[idx].Instances {
+			fmt.Println("    - Instance ID: ", *inst.InstanceId)
+		}
+	}
+
+	fmt.Println("AA")
+}
