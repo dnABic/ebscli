9a19d15b51afa659b164f7660af277233b97f14b
dnABic (Andreja Babic) <ababic@gmail.com>
2016-12-29 01:18:32 +0100

    get volume id from attached volumes

--- ebscli.go
+++ ebscli.go
@@ -26,8 +26,13 @@ func main() {
 		fmt.Println("  > Number of instances: ", len(res.Instances))
 		for _, inst := range resp.Reservations[idx].Instances {
 			fmt.Println("    - Instance ID: ", *inst.InstanceId)
+			fmt.Println("Block devices count", len(inst.BlockDeviceMappings))
+			for cnt, ebs := range inst.BlockDeviceMappings {
+				fmt.Println(cnt)
+				fmt.Println("Block device name", *ebs.DeviceName)
+				volume := ebs.Ebs
+				fmt.Println(*volume.VolumeId)
+			}
 		}
 	}
-
-	fmt.Println("AA")
 }
