9ac2284d336e2a93587e35941c0e02215c043052
dnABic (Andreja Babic) <ababic@gmail.com>
2017-03-04 23:33:41 +0100

    added endpoints for aws provider for terraform file

--- integration_test/terraform/aws_setup/main.tf
+++ integration_test/terraform/aws_setup/main.tf
@@ -1,4 +1,15 @@
-provider "aws" {}
+provider "aws" {
+  endpoints {
+    s3  = "http://awsserver:5000"
+    ec2 = "http://awsserver:5000"
+    iam = "http://awsserver:5000"
+    elb = "http://awsserver:5000"
+  }
+
+  access_key = "the_key"
+  secret_key = "the_secret"
+  region     = "us-east-1"
+}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
