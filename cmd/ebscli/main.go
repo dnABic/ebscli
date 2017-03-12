d82e9227d7790d40d4983675d6c51b1f244ddb5b
dnABic (Andreja Babic) <ababic@gmail.com>
2017-03-12 20:46:54 +0100

    Adding version command

--- cmd/ebscli/main.go
+++ cmd/ebscli/main.go
@@ -4,9 +4,10 @@ import (
 	"os"
 
 	"github.com/dnabic/ebscli"
+	"github.com/dnabic/ebscli/version"
 )
 
 func main() {
-	exitCode := ebscli.Main(os.Args)
+	exitCode := ebscli.Main(os.Args, version.GetVersion())
 	os.Exit(exitCode)
 }
